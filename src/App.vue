eb7ccaff0e6099596200d40cf379e185cac50701
田世洋 <tianshiyang@icourt.cc>
2026-04-06 19:06:37 +0800

    feat: netify配置

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { ConfigProvider } from 'ant-design-vue'
 import zhCN from 'ant-design-vue/es/locale/zh_CN'
-// throw Error('11111')
+
+const handleError = () => {
+  throw Error("项目报错了")
+}
 </script>
 
 <template>
+  <a-button @click="handleError">报错</a-button>
   <ConfigProvider :locale="zhCN">
     <router-view />
   </ConfigProvider>
