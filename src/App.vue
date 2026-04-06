94bf64c42ddea30587bf5e971d7df847d554f074
田世洋 <tianshiyang@icourt.cc>
2026-04-06 23:50:06 +0800

    feat: 问题修复

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,15 @@ import zhCN from 'ant-design-vue/es/locale/zh_CN'
 const handleError = () => {
   throw Error('项目报错了')
 }
+
+const handleError2 = () => {
+  throw Error('项目报错了')
+}
 </script>
 
 <template>
   <a-button @click="handleError">报错</a-button>
+  <a-button @click="handleError2">报错2</a-button>
   <ConfigProvider :locale="zhCN">
     <router-view />
   </ConfigProvider>
